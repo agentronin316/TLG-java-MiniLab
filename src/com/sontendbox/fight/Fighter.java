0b4ea629ad97503fc2a17fce59a411f31bb8fb96
agentronin316 <agentronin316@users.noreply.github.com>
2022-04-04 12:23:27 -0500

    added Attack and Weapon enums with base functions

--- src/com/sontendbox/fight/Fighter.java
+++ src/com/sontendbox/fight/Fighter.java
@@ -3,6 +3,10 @@ package com.sontendbox.fight;
 import java.util.Arrays;
 
 class Fighter {
+    private static final String missMessage = "misses.";
+    private static final String hitFormat = "%s for %s damage!";
+    private static final String exceptionMessageFormat = "weapon %s does not have attack: %s";
+
     private Weapon weapon;
     private int health = 100;
 
@@ -17,15 +21,15 @@ class Fighter {
     String attack(Attack attack, Fighter opponent) throws IllegalArgumentException{
         String toReturn;
         if(Arrays.stream(weapon.getAttacks()).noneMatch(attack1 -> attack1 == attack)){
-            throw new IllegalArgumentException("weapon " + weapon + " does not have attack: " + attack);
+            throw new IllegalArgumentException(String.format(exceptionMessageFormat, weapon.name(), attack.name()));
         }
         int hit = (int)(Math.random() * 100);
         if (hit < attack.getAccuracy()){
             int damage = weapon.damageCalculation(attack);
             opponent.takeDamage(damage);
-            toReturn = weapon.getVerb() + " for " + damage + " damage!";
+            toReturn = String.format(hitFormat, weapon.getVerb(), damage);
         } else {
-            toReturn = "misses.";
+            toReturn = missMessage;
         }
         return toReturn;
     }
@@ -38,7 +42,8 @@ class Fighter {
         return health;
     }
 
-    private void takeDamage(int damage) {
+    private boolean takeDamage(int damage) {
         health -= damage;
+        return (health < 1);
     }
 }
