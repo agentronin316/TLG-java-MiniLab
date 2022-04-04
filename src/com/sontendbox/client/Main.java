ae993f1cc7318dcef45de36ee20b8a5cba63917d
wkkobus <willkobus@gmail.com>
2022-04-04 12:09:23 -0400

    created controller and main classes. package com.sontendbox.client

---
+++ src/com/sontendbox/client/Main.java
@@ -0,0 +1,9 @@
+package com.sontendbox.client;
+
+class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.execute();
+    }
+
+}
