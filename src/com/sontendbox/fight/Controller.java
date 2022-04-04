fa144ed2213a1f98194dfd0c8d34d925b868af96
wkkobus <willkobus@gmail.com>
2022-04-04 13:48:17 -0400

    worked on controller

--- src/com/sontendbox/fight/Controller.java
+++ src/com/sontendbox/fight/Controller.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Controller {
-    //private Fighter combantant1;
-    //private Fighter combantant2;
+    private Fighter combantant1;
+    private Fighter combantant2;
     private boolean isHuman;
     private Scanner scanner = new Scanner(System.in);
+    private Weapon weapon;
+    private int numPlayers = 0;
 
     public void execute() {
         greet();
@@ -22,34 +24,53 @@ public class Controller {
     }
 
     private void movePrompt() {
-        System.out.println("Select Attack");
-        String attack = scanner.nextLine();
-        System.out.println(attack);
     }
 
     private void weaponSelection() {
-        System.out.print("Select weapon. Current weapon availability is limited to : " + Weapon.FIST.getPrintName());
-        String weapon = scanner.next();
-        System.out.println(weapon);
+        int i = 1;
+        while(i <= 2) {
+            System.out.print("Select weapon. Current weapon availability is limited to "
+                    + Weapon.FIST.getPrintName());
+            String input = scanner.next();
+            for (Weapon weapon : Weapon.values()) {
+                if (input.equalsIgnoreCase(weapon.getPrintName()))
+                    this.weapon = weapon;
+                System.out.println("Weapon chosen: " + weapon.getPrintName());
+            }
+            switch (i){
+                case 1:
+                    combantant1 = new Fighter(weapon);
+                    i++;
+                    break;
+                case 2:
+                    combantant2 = new Fighter(weapon);
+                    i++; break;
+            }
+        }
     }
 
     private void numPlayersPrompt() {
-        System.out.print("Enter number of players (0-2) players allowed: ");
-        boolean isValid = false;
+        boolean isValid = true;
         while (isValid) {
-            int numPlayers = Integer.parseInt(scanner.nextLine());
-            if(numPlayers < 0)
+            System.out.print("Enter number of players (0-2) players allowed: ");
+            this.numPlayers = Integer.parseInt(scanner.nextLine());
+            if(numPlayers >= 0 && numPlayers <=2) {
 
-            switch (numPlayers) {
-                case 0:
-                    System.out.println("Battle is between 2 computer controlled fighters");
-                    break;
-                case 1:
-                    System.out.println("Battle is between 1 human controlled fighter and 1 computer controlled fighter ");
-                    break;
-                case 2:
-                    System.out.println("Battle is between 2 human controlled fighters");
-                    break;
+                switch (numPlayers) {
+                    case 0:
+                        System.out.println("Battle is between 2 computer controlled fighters");
+
+                        isValid = false;
+                        break;
+                    case 1:
+                        System.out.println("Battle is between 1 human controlled fighter and 1 computer controlled fighter ");
+                        isValid = false;
+                        break;
+                    case 2:
+                        System.out.println("Battle is between 2 human controlled fighters");
+                        isValid = false;
+                        break;
+                }
             }
         }
     }
