084c60e89f9aed3781b68cc976dbe6461394ce85
agentronin316 <agentronin316@users.noreply.github.com>
2022-04-05 10:51:23 -0500

    Implemented one additional attack and four additional weapons

--- src/com/sontendbox/fight/Attack.java
+++ src/com/sontendbox/fight/Attack.java
@@ -2,7 +2,8 @@ package com.sontendbox.fight;
 
 enum Attack {
     BALANCED(70, 3),
-    ACCURATE(90, 0);
+    ACCURATE(90, 0),
+    HEAVY(50, 9);
 
     private int accuracy;
     private int damageMod;
