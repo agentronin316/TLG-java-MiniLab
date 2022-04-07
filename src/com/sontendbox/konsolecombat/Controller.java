e553bdb490afc94a37cd1e8c1348ef066c9d45ff
agentronin316 <agentronin316@users.noreply.github.com>
2022-04-07 11:05:10 -0500

    fleshed out comments on code and renamed some variables and methods

--- src/com/sontendbox/konsolecombat/Controller.java
+++ src/com/sontendbox/konsolecombat/Controller.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Controller {
+    //file path constants
     private static final String GREET_FILE_PATH = "resources/greeting.txt";
     private static final String WEAPON_TIP_FILE_PATH = "resources/weaponHelp.txt";
     private static final String CHARACTER_TIP_FILE_PATH = "resources/characterHelp.txt";
@@ -18,7 +19,7 @@ public class Controller {
     private static final String SWORD_PLAYER_FILE_PATH = "resources/swordPlayer.txt";
     private static final String MACE_PLAYER_FILE_PATH = "resources/macePlayer.txt";
 
-
+    //all other constants
     private static final String weaponSelectPrompt = "Select weapon. Weapons to chose from are: \n";
     private static final String weaponSelectPrompt2 = "Enter the name of a weapon listed above \n";
     private static final String selectCharacter = "Select character. Available characters are: ";
@@ -30,9 +31,12 @@ public class Controller {
             "1 computer controlled fighter";
     private static final String zeroPlayerAnnouncement = "Battle is between 2 computer controlled fighters";
     private static final String namePrompt = "Enter a name for player: ";
-    private static Controller instance;
-    private static CombatSubController combatContol;
 
+    //static variable
+    private static CombatSubController combatControl;
+
+            //#### Singleton stuff
+    private static Controller instance;
 
     public static Controller getInstance() {
         if (instance == null) {
@@ -43,56 +47,60 @@ public class Controller {
     }
 
     private Controller() {
-        combatContol = CombatSubController.getInstance();
-        combatContol.setScanner(scanner);
+        combatControl = CombatSubController.getInstance();
+        combatControl.setScanner(scanner);
     }
+            //#### End singleton stuff
 
+    //instance variable
     private Scanner scanner = new Scanner(System.in);
 
+    //main game loop
     public void execute() {
         boolean playAgain = true;
         while (playAgain) {
-
             show(GREET_FILE_PATH);
             numPlayersPrompt();
-            weaponSelection();
-
-            playAgain = combatContol.runCombat();
+            chooseCombatants();
+            playAgain = combatControl.runCombat(); //delegate combat to combat controller and ask for another game
 
         }
     }
 
-    private void weaponSelection() {
-        Fighter combatant = getCombatant();
-        combatContol.setCombatant1(combatant);
+    private void chooseCombatants() {
+        Fighter combatant = createCombatant();
+        combatControl.setCombatant1(combatant); //hand the combatant off to the combat controller
+        displayPlayer(combatant.getWeaponName()); //show character picture
         System.out.print("\t\t\t\t" +combatant.getName() + CHOSE + combatant.getWeaponName() + " and "
-                + combatant.getCharacter().getPrintName()+ "\n");
-        displayPlayer(combatant.getWeaponName());
+                + combatant.getCharacter().getPrintName()+ "\n"); //summarize choice of combatant
         System.out.println();
-        combatant = getCombatant();
-        combatContol.setCombatant2(combatant);
+        combatant = createCombatant(); //and repeat for the second combatant
+        combatControl.setCombatant2(combatant);
+        displayPlayer(combatant.getWeaponName());
         System.out.println(combatant.getName() + CHOSE + combatant.getWeaponName() + " and "
                 + combatant.getCharacter().getPrintName());
-        displayPlayer(combatant.getWeaponName());
         System.out.println();
     }
 
-    private Fighter getCombatant() {
+    private Fighter createCombatant() {
+        //prompt for name
         System.out.print(namePrompt);
         String name = scanner.next();
         Console.clear();
+        //prompt for weapon
         StringBuilder builder = new StringBuilder();
         show(WEAPON_TIP_FILE_PATH);
         System.out.println();
         builder.append(weaponSelectPrompt);
         Weapon[] weapons = Weapon.values();
         Character[] characters = Character.values();
-        for (int i = 0; i < weapons.length - 1; i++) {
+        for (int i = 0; i < weapons.length - 1; i++) { //make a list of weapons with attacks
             builder.append(weapons[i].getPrintName()).append(Arrays.toString(weapons[i].getAttacks()));
             builder.append(", ");
             builder.append("\n");
         }
-        builder.append(weapons[weapons.length - 1].getPrintName()).append(Arrays.toString(weapons[weapons.length - 1].getAttacks()));
+        builder.append(weapons[weapons.length - 1].getPrintName());
+        builder.append(Arrays.toString(weapons[weapons.length - 1].getAttacks()));
         System.out.print(builder);
         System.out.println();
         System.out.println();
@@ -126,6 +134,7 @@ public class Controller {
         }
     }
 
+    //prompt for number of players, fill in the rest with computer controlled characters
     private void numPlayersPrompt() {
         boolean isInvalid = true;
         while (isInvalid) {
@@ -136,20 +145,20 @@ public class Controller {
                 switch (numPlayers) {
                     case 0:
                         System.out.println(zeroPlayerAnnouncement);
-                        combatContol.setC1Human(false);
-                        combatContol.setC2Human(false);
+                        combatControl.setC1Human(false);
+                        combatControl.setC2Human(false);
                         System.out.println();
                         break;
                     case 1:
                         System.out.println(onePlayerAnnouncement);
-                        combatContol.setC1Human(true);
-                        combatContol.setC2Human(false);
+                        combatControl.setC1Human(true);
+                        combatControl.setC2Human(false);
                         System.out.println();
                         break;
                     case 2:
                         System.out.println(twoPlayerAnnouncement);
-                        combatContol.setC1Human(true);
-                        combatContol.setC2Human(true);
+                        combatControl.setC1Human(true);
+                        combatControl.setC2Human(true);
                         System.out.println();
                         break;
                 }
@@ -157,6 +166,7 @@ public class Controller {
         }
     }
 
+    //displays a text file at @path
     void show(String path) {
         try {
             Files.lines(Path.of(path))
@@ -165,8 +175,10 @@ public class Controller {
             e.printStackTrace();
         }
     }
-    void displayPlayer(String name){
-        switch (name){
+
+    //displays character models with weapons
+    void displayPlayer(String weaponName){
+        switch (weaponName){
             case "bow":
                 show(BOW_PLAYER_FILE_PATH);
                 break;
--- src/com/sontendbox/konsolecombat/Weapon.java
+++ src/com/sontendbox/konsolecombat/Weapon.java
@@ -3,15 +3,11 @@ package com.sontendbox.konsolecombat;
 import java.util.Arrays;
 
 enum Weapon {
-
-    
-
     FIST("punches", 8, "fist", Attack.BALANCED),
     BOW("shoots", 8, "bow", Attack.ACCURATE),
     SWORD("slashes", 8, "sword", Attack.ACCURATE, Attack.BALANCED),
     MACE("bashes", 8, "mace", Attack.BALANCED, Attack.HEAVY),
     BATTLEAXE("hacks", 8, "battleaxe", Attack.HEAVY);
-
 
     private final String verb;
     private final int baseDamage;
@@ -37,6 +33,10 @@ enum Weapon {
         return verb;
     }
 
+    int damageCalculation(Attack attack) {
+        return baseDamage + attack.getDamageMod();
+    }
+
     @Override
     public String toString() {
         return getClass().getSimpleName() + ": "
@@ -44,9 +44,5 @@ enum Weapon {
                 +", baseDamage=" + baseDamage
                 + ", printName='" + getPrintName()
                 +", attacks=" + Arrays.toString(attacks);
-    }
-
-    int damageCalculation(Attack attack) {
-        return baseDamage + attack.getDamageMod();
     }
 }
